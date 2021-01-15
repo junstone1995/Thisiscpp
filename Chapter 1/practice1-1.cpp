6daaf923bf63b8f4d5c245da1bf957dd520fdf0b
이준석 <boris0730@gmail.com>
2021-01-15 17:13:41 +0900

    Chapter 1

---
+++ Chapter 1/practice1-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int     main()
+{
+    string name;
+    string age;
+
+    cin >> name >> age;
+    cout << "나의 이름은 " << name << "이고," << age << "살 입니다." << endl;
+
+    return (0);
+}
---
+++ Chapter 1/practice1-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int		main()
+{
+	char *lee = new char[12]{"Hello, World!"};
+
+
+}
